1c11580626ad3a0ec65fc073edd66acbffef1c03
bellsource <belubascur@gmail.com>
2020-11-28 16:48:49 -0300

    ejemplo de body-parser comentado para impletar un middelware externo

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require ("express");
 const server = express();
 
+//si queremos implementar un middelware externo como body-parser
+/**
+ * Hay q tener instalada la dependencia
+ * >>npm i body-parser
+ * 
+ * llamamos
+ * >> const bodyParser = require ("body-parser");
+ * 
+ * utilizamos
+ * >> server.use(bodyParser.json());
+ * 
+ * se puede acceder desde el body ej
+ * req.body.nombre
+ * 
+ * >> server.post("/contact", (req,res) =>{
+ * >>    console.log(req.body);
+ * >>    res.json('contacto agregado!');      
+ * >> });
+ * 
+ * MANEJO DE ERRORES
+ * 
+ * >> server.post('/contact', (req,res) => {
+ * >>    const{nombre, apellido} = req.body;
+ * >>    if (!nombre || apeliido){
+ * >>       res.status(400)
+ * >>       .json('falta información');
+ * >>    }else{
+ * >>       res.json('contacto agregado');
+ * >>       }
+ * >> });
+ */
+
+
 //si en la req viene el usuario belen hace un next 
 //sino en el res devuelve un statusCode(401)
 
